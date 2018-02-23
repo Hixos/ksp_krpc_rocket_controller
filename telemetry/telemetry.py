0a4746540a6935595cdfe5cf1d5fc8f67a9da6c9
Hixos <luca.erbetta105@gmail.com>
2018-02-24 00:19:51 +0100

    [ADD|WIP] basic telemetry implementation pt 4

--- telemetry/telemetry.py
+++ telemetry/telemetry.py
@@ -1,10 +1,13 @@
 from fsm.fsm import StateMachine
+
+from global_streams import global_streams
+
 telemetry_unit = {}
 
 
 class GroupBuilder:
-    def __init__(self, groupname):
-        self.groupname = groupname
+    def __init__(self, group_name):
+        self.group_name = group_name
         self.data = {}
 
     def addData(self, key, value):
@@ -12,7 +15,7 @@ class GroupBuilder:
         return self
 
     def build(self):
-        return self.groupname + "_group", self.data
+        return self.group_name + "_group", self.data
 
 
 def addGroup(group):
@@ -22,8 +25,14 @@ def addGroup(group):
 def update(state_machine):
     collect(state_machine)
 
+    print(telemetry_unit)
+
 
 def collect(state_machine: StateMachine):
+    basic = GroupBuilder('basic')
+    basic.addData('ut', global_streams.ut())
+
+    addGroup(basic.build())
     addGroup(state_machine.collectTelemetry())
 
 
